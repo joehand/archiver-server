8bcd7ceadf9f54db922a3947ce2007cb413cf540
Joe Hand <joe@joeahand.com>
2016-11-03 11:26:57 -0700

    add files

---
+++ index.js
@@ -0,0 +1,33 @@
+var Swarm = require('discovery-swarm')
+var swarmDefaults = require('datland-swarm-defaults')
+
+module.exports = function (archiver, opts) {
+  if (!archiver) throw new Error('hypercore archiver required')
+  if (!opts) opts = {}
+
+  var swarm = Swarm(swarmDefaults({
+    utp: opts.utp || true,
+    tcp: opts.tcp || true,
+    stream: function () {
+      return archiver.replicate()
+    }
+  }))
+  swarm.once('error', function () {
+    swarm.listen(0)
+  })
+  swarm.listen(opts.port || 3282)
+
+  archiver.list().on('data', serveArchive)
+  archiver.on('add', serveArchive)
+  archiver.on('remove', function (key) {
+    swarm.leave(archiver.discoveryKey(key))
+  })
+
+  return
+
+  function serveArchive (key) {
+    console.log(`replicating ${key.toString('hex')} over Dat`)
+    // random timeout so it doesn't flood DHT
+    setTimeout(swarm.join(archiver.discoveryKey(key)), Math.floor(Math.random() * 30 * 1000))
+  }
+}
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Archiver-Server
+
+Serve keys found in a [hypercore-archiver](https://github.com/mafintosh/hypercore-archiver) with `discovery-swarm`.
+
+## Usage
+
+Serve archives in a hypercore-archiver with discovery-swarm.
+
+```js
+var Archiver = require('hypercore-archiver')
+var server = require('archiver-server')
+
+var archives = Archiver({dir: process.cwd()})
+server(archives)
+
+// Any archives added will be available over discovery-swarm network
+archives.add(key)
+```
+
+## API
+
+### server(archiver, [opts])
+
+Start serving archives. Will list existing keys to serve and serve new keys when added.
+
+```js
+opts = {
+  utp: true,
+  tcp: true,
+  port: 3282
+}
+```
+
+## TODO:
+
+* Serve over http with hyperdrive-http
+
+## License
+
+MIT
